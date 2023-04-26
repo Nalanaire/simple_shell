b965fc7f2a872357644ce5581fa042f527b00cf2
Nalanaire <senzonala@yahoo.com>
2023-04-26 07:17:26 -0700

    Simple shell

--- builtinB.c
+++ builtinB.c
@@ -28,7 +28,7 @@ int _malias(info_t *info)
 		if (p)
 			set_alias(info, info->argv[i]);
 		else
-			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+			print_list(node_starts_with(info->alias, info->argv[i], '='));
 	}
 
 	return (0);
